78b8858c6b3d826121bbac344fd96801dd3f9217
Beomman-Han <qjaaks6378@gmail.com>
2022-03-10 17:58:37 +0900

    Init RosalindSolver class

---
+++ RosalindSolver.py
@@ -0,0 +1,8 @@
+class RosalindSolver:
+    """Class for solving 'Rosalind' bioinformatics problems.
+    Each method represents each Rosalind problem.
+    """
+    
+    def __init__(self):
+        pass
+    
