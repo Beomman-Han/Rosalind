51df573d18c6bd4a21944fcf7ce2bb0a31a2ab77
Beomman-Han <qjaaks6378@gmail.com>
2022-05-12 23:57:25 +0900

    Add test code for parse func of pkgs/FASTA.py module

---
+++ tests/test_FASTA.py
@@ -0,0 +1,23 @@
+import os, sys
+sys.path.append('./rosalind')
+
+from pkgs import FASTA
+
+import unittest
+
+class TestFASTA(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.fasta = './rosalind/problems/overlap_graph_case1.fasta'
+        self.ids = ['Rosalind_0498', 'Rosalind_2391',
+                    'Rosalind_2323', 'Rosalind_0442']
+        self.seqs = ['AAATAAA\n', 'AAATTTT\n',
+                     'TTTTCCC\n', 'AAATCCC\n']
+    
+    def test_parse(self) -> None:
+        for idx, seq_record in enumerate(FASTA.parse(self.fasta)):
+            self.assertEqual(seq_record.id, self.ids[idx])
+            self.assertEqual(seq_record.seq, self.seqs[idx])
+        
+if __name__ == "__main__":
+    unittest.main()
