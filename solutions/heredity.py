e1fcb39df3b248a3c08183a3b3c2988590a03da4
Beomman-Han <qjaaks6378@gmail.com>
2022-06-14 23:14:24 +0900

    Add solution for calculating prob of 'AaBb' organisms
    Use binomial distribution where X is random variable ~ B(2^k, 0.25)

--- solutions/heredity.py
+++ solutions/heredity.py
@@ -16,6 +16,27 @@ def calculate_expected_offspring(
     
     return expected_dominant_offspring
 
+def independent_alleles(
+    generation : int,
+    least_num : int
+    ) -> float:
+    
+    num_population = 2 ** generation
+    prob = 0.0
+    for i in range(least_num, num_population + 1):
+        prob += _get_factorial(num_population) / \
+            (_get_factorial(num_population - i) * _get_factorial(i)) * \
+                (0.25 ** i) * (0.75 ** (num_population - i))
+    return prob
+
+def _get_factorial(n : int) -> int:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial
+    
+
 if __name__ == '__main__':
     ## solution
     print(calculate_expected_offspring([19785, 16489, 18613, 19160, 17014, 19831]))
+    print(independent_alleles(5, 7))
